8aa33bfaf5807282db0288ec245a11f54b228465
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2024-04-20 18:59:18 -0300

    Add license

--- android/src/main/java/com/github/douglasjunior/reactNativeAppLifecycle/AppLifecyclePackage.java
+++ android/src/main/java/com/github/douglasjunior/reactNativeAppLifecycle/AppLifecyclePackage.java
@@ -1,6 +1,6 @@
 // MIT License
 
-// Copyright (c) 2023 Douglas Nassif Roma Junior
+// Copyright (c) 2024 Douglas Nassif Roma Junior
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
